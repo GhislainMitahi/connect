ce788aba182d929c4aa47016044d7b37f0bdd713
christian <christianbiringanine22@gmail.com>
2024-07-31 12:24:59 +0200

    Initialize different links misions

---
+++ app/(protected)/co-creator/page.tsx
@@ -0,0 +1,13 @@
+import DashboardLayout from "../dashboard/layout";
+
+const CoCreatorPage = () => {
+  return (
+    <DashboardLayout>
+      <div>
+        <h1>Co-Creator Layout</h1>
+      </div>
+    </DashboardLayout>
+  );
+}
+ 
+export default CoCreatorPage;
