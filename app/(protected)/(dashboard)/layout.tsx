bb44ea0638b61efd04f17a075c46e949e65b4e35
Ghislain Mitahi <ghislainmitahi@gmail.com>
2024-08-21 04:21:54 +0300

    feat(review): no workflow detected

--- app/(protected)/(dashboard)/layout.tsx
+++ app/(protected)/(dashboard)/layout.tsx
@@ -1,15 +1,20 @@
 "use client";
 
-import React, { useEffect, useState, useRef } from "react";
-import { useWindowSize } from "@/app/hooks/useWindowsSize";
 import useClickOutside from "@/app/hooks/useClickOutside";
+import { useWindowSize } from "@/app/hooks/useWindowsSize";
 import { poppins } from "@/lib/fonts";
-import { AlignLeftOutlined, LeftCircleFilled, RightCircleFilled } from "@ant-design/icons";
+import {
+  AlignLeftOutlined,
+  LeftCircleFilled,
+  RightCircleFilled,
+} from "@ant-design/icons";
 import { Avatar, Button, Layout, Menu, Radio, RadioChangeEvent } from "antd";
+import React, { useEffect, useRef, useState } from "react";
 
 import { CustomSession } from "@/app/types/next-auth";
 import MenuElement from "@/components/MenuElement";
 import Profile from "@/components/Profile";
+import ModalSetting from "@/components/profileModal";
 import Bolt from "@/components/svg/Bolt";
 import Code from "@/components/svg/code";
 import Connection from "@/components/svg/connection";
@@ -26,7 +31,6 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { useSession } from "next-auth/react";
-import ModalSetting from "@/components/profileModal";
 
 const { Header, Sider, Content } = Layout;
 
@@ -80,13 +84,12 @@ export default function DashboardLayout({
   };
 
   const handleCloseModal = () => {
-    setMobileSetting(!mobileSetting)
-  }
+    setMobileSetting(!mobileSetting);
+  };
 
   useClickOutside(ModalRef, handleCloseModal);
   //It's giving some bugs when using the click outside hook on the mobile menu: TO-DO
   // useClickOutside(MenuRef, handleToggleDrawer)
-
 
   const menuData: menuItems[] = [
     {
@@ -184,115 +187,121 @@ export default function DashboardLayout({
   ];
 
   return (
-    <main className="h-full">
-      <div className="px-4 pr-6 sticky z-10 bg-sidebarcolor top-0 shadow-md flex justify-between items-center md:hidden" ref={MenuRef}>
+    <main className="min-h-screen">
+      <div
+        className="px-4 pr-6 sticky z-10 bg-sidebarcolor top-0 shadow-md flex justify-between items-center md:hidden"
+        ref={MenuRef}
+      >
         <div
           className="flex text-xs items-center justify-between gap-2 text-linkColor py-4"
           onClick={() => setMobileSetting(!mobileSetting)}
         >
-            <Avatar />
-            <div>
-              <p className="text-sm font-medium ">Creator</p>
-              <p className="">{session?.user?.name}</p>
-            </div>
-          { mobileSetting && (
-          <div ref={ModalRef}>
-            <ModalSetting />
+          <Avatar />
+          <div>
+            <p className="text-sm font-medium ">Creator</p>
+            <p className="">{session?.user?.name}</p>
           </div>
+          {mobileSetting && (
+            <div ref={ModalRef}>
+              <ModalSetting />
+            </div>
           )}
-       </div>
-        <AlignLeftOutlined className="text-linkColor" onClick={handleToggleDrawer}/>
+        </div>
+        <AlignLeftOutlined
+          className="text-linkColor"
+          onClick={handleToggleDrawer}
+        />
       </div>
       <div
         className={`md:hidden fixed top-17 z-10 right-0 pt-2 px-3 h-full w-64 bg-sidebarcolor shadow-lg transform transition-transform ${
-          open ? 'translate-x-0' : 'translate-x-full'
+          open ? "translate-x-0" : "translate-x-full"
         }`}
       >
-          <div className="flex flex-col h-full pt-4 px-4 overflow-y-auto">
-            <div className="flex flex-col gap-2 w-4/5">
-              {menuData.map((item, index) => (
+        <div className="flex flex-col h-full pt-4 px-4 overflow-y-auto">
+          <div className="flex flex-col gap-2 w-4/5">
+            {menuData.map((item, index) => (
+              <MenuElement
+                key={index}
+                icon={item.icon}
+                title={item.title}
+                url={item.url}
+                label={item.label}
+                isCollapsed={false}
+                disabled={item.disabled}
+                toggleDrawer={handleToggleDrawer}
+              />
+            ))}
+          </div>
+          <div className="mt-14">
+            <section className="text-linkColor mb-8">
+              <h2 className="mb-1 font-semibold text-xs">Account</h2>
+              <div className="flex flex-col gap-1 px-1">
                 <MenuElement
-                  key={index}
-                  icon={item.icon}
-                  title={item.title}
-                  url={item.url}
-                  label={item.label}
+                  icon={
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <NotificationBell />
+                        </TooltipTrigger>
+                        <TooltipContent className="bg-sidehover border border-slate-500">
+                          <p className="text-linkColor">Notification</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  }
+                  title="Notifications"
+                  url="/notifications"
+                  key={989898989898989}
                   isCollapsed={false}
-                  disabled={item.disabled}
                   toggleDrawer={handleToggleDrawer}
                 />
-              ))}
-            </div>
-            <div className="mt-14">
-              <section className="text-linkColor mb-8">
-                <h2 className="mb-1 font-semibold text-xs">Account</h2>
-                <div className="flex flex-col gap-1 px-1">
-                  <MenuElement
-                    icon={
-                      <TooltipProvider>
-                        <Tooltip>
-                          <TooltipTrigger>
-                            <NotificationBell />
-                          </TooltipTrigger>
-                          <TooltipContent className="bg-sidehover border border-slate-500">
-                            <p className="text-linkColor">Notification</p>
-                          </TooltipContent>
-                        </Tooltip>
-                      </TooltipProvider>
-                    }
-                    title="Notifications"
-                    url="/notifications"
-                    key={989898989898989}
-                    isCollapsed={false}
-                    toggleDrawer={handleToggleDrawer}
-                  />
-                  <MenuElement
-                    icon={
-                      <TooltipProvider>
-                        <Tooltip>
-                          <TooltipTrigger>
-                            <SettingIcon />
-                          </TooltipTrigger>
-                          <TooltipContent className="bg-sidehover border border-slate-500">
-                            <p className="text-linkColor">Settings</p>
-                          </TooltipContent>
-                        </Tooltip>
-                      </TooltipProvider>
-                    }
-                    title="Settings"
-                    url="/settings"
-                    key={9009787865}
-                    isCollapsed={false}
-                    toggleDrawer={handleToggleDrawer}
-                  />
-                </div>
-              </section>
-                <section>
-                  <Radio.Group
-                    onChange={handleModeChange}
-                    className="flex items-center justify-center mb-4"
-                    value={mode}
-                    optionType="button"
-                    buttonStyle="solid"
-                  >
-                    <Radio.Button value="top" className="bg-[#ECF8CB]">
-                      <div className="flex items-center gap-2">
-                        <Code />
-                        <p>Test</p>
-                      </div>
-                    </Radio.Button>
-                    <Radio.Button value="left" className="bg-[#ECF8CB]">
-                      <div className="flex items-center gap-2">
-                        <Bolt />
-                        <p>Live</p>
-                      </div>
-                    </Radio.Button>
-                  </Radio.Group>
-                </section>
-            </div>
+                <MenuElement
+                  icon={
+                    <TooltipProvider>
+                      <Tooltip>
+                        <TooltipTrigger>
+                          <SettingIcon />
+                        </TooltipTrigger>
+                        <TooltipContent className="bg-sidehover border border-slate-500">
+                          <p className="text-linkColor">Settings</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </TooltipProvider>
+                  }
+                  title="Settings"
+                  url="/settings"
+                  key={9009787865}
+                  isCollapsed={false}
+                  toggleDrawer={handleToggleDrawer}
+                />
+              </div>
+            </section>
+            <section>
+              <Radio.Group
+                onChange={handleModeChange}
+                className="flex items-center justify-center mb-4"
+                value={mode}
+                optionType="button"
+                buttonStyle="solid"
+              >
+                <Radio.Button value="top" className="bg-[#ECF8CB]">
+                  <div className="flex items-center gap-2">
+                    <Code />
+                    <p>Test</p>
+                  </div>
+                </Radio.Button>
+                <Radio.Button value="left" className="bg-[#ECF8CB]">
+                  <div className="flex items-center gap-2">
+                    <Bolt />
+                    <p>Live</p>
+                  </div>
+                </Radio.Button>
+              </Radio.Group>
+            </section>
+          </div>
         </div>
       </div>
-      <Layout className={`min-h-screen relative`}>
+      <Layout className={`h-screen relative`}>
         <Sider
           trigger={null}
           collapsible
@@ -300,7 +309,7 @@ export default function DashboardLayout({
           style={{
             background: "#ecf8cb",
           }}
-          className="hidden md:block fixed top-0 left-0 h-screen bg-red-400"
+          className="hidden md:block fixed top-0 left-0 bottom-0 h-full"
         >
           <div className="h-full bg-sidebarcolor flex flex-col gap-6 px-4 border">
             <div className="flex justify-center items-center">
@@ -406,7 +415,10 @@ export default function DashboardLayout({
           </div>
         </Sider>
         <Layout style={{ backgroundColor: "#f2ffe9" }}>
-          <Header style={{ padding: 0, background: "#f2ffe9" }} className="hidden md:block">
+          <Header
+            style={{ padding: 0, background: "#f2ffe9" }}
+            className="hidden md:block"
+          >
             <div className="hidden md:flex items-center justify-between">
               <Button
                 type="text"
--- components/chat.tsx
+++ components/chat.tsx
@@ -1,26 +1,74 @@
-import { AudioOutlined, CopyOutlined, PaperClipOutlined, SyncOutlined,
-  PictureOutlined, SendOutlined, ShareAltOutlined, EllipsisOutlined
-}
-  from "@ant-design/icons";
+import {
+  AudioOutlined,
+  CopyOutlined,
+  EllipsisOutlined,
+  PaperClipOutlined,
+  PictureOutlined,
+  SendOutlined,
+  SyncOutlined,
+} from "@ant-design/icons";
 import { Avatar } from "antd";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+import { FormSchemaChat } from "@/lib/zodSchema";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Textarea } from "@/components/ui/textarea";
+import { toast } from "@/components/ui/use-toast";
+
 const VisionUserChat = () => {
+  const form = useForm<z.infer<typeof FormSchemaChat>>({
+    resolver: zodResolver(FormSchemaChat),
+  });
+
+  function onSubmit(data: z.infer<typeof FormSchemaChat>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    });
+  }
+
   return (
-    <div className="w-full flex flex-col items-center md:items-start md:justify-center md:w-[80%] md:mx-auto">
-      <div className="bg-sidebarcolor min-h-52 rounded-lg py-6 px-4 my-6">
-        <div className="flex justify-between gap-4">
-          <Avatar className=""/>
+    <div className="w-full flex flex-col items-center">
+      <div className="bg-sidebarcolor min-h-52 rounded-lg p-4 md:w-[80%]">
+        <div className="flex gap-4">
+          <Avatar className="" />
           <div className="flex gap-2">
             <div>
-              <p className="text-auth-text-color font-bold text-[13px] ">VISION</p>
+              <p className="text-auth-text-color font-bold text-[13px] ">
+                VISION
+              </p>
               {/* <small>Today 2:45 PM</small> */}
               <p>Thank you your instagram is connected now</p>
               <p>Analyzing ...</p>
-              <p>Analysis completed! Here's an overview of your recent performance:</p>
+              <p>
+                Analysis completed! Here's an overview of your recent
+                performance:
+              </p>
               <ul>
                 <li>Engagment rate : 4.5%</li>
-                <li>Top performaing Post : Your recent travel blog with 1200 likes and 300 comments</li>
-                <li>Audience Demographics: Predominantly aged 18-24 with a strong following from New York and Los Angeles</li>
+                <li>
+                  Top performaing Post : Your recent travel blog with 1200 likes
+                  and 300 comments
+                </li>
+                <li>
+                  Audience Demographics: Predominantly aged 18-24 with a strong
+                  following from New York and Los Angeles
+                </li>
               </ul>
               <div className="flex mt-4 gap-4">
                 <CopyOutlined />
@@ -31,31 +79,44 @@ const VisionUserChat = () => {
           </div>
         </div>
       </div>
-      <div className="rounded-lg py-6 px-4 mb-4">
-        <div className="flex justify-between gap-4">
-          <Avatar className=""/>
+      <div className="rounded-lg py-6 px-4 mb-4 md:w-[80%]">
+        <div className="flex gap-4">
+          <Avatar className="" />
           <div className="flex gap-2">
             <div>
-              <p className="text-auth-text-color font-bold text-[13px] ">Alfred Wandjau</p>
+              <p className="text-auth-text-color font-bold text-[13px] ">
+                Alfred Wandjau
+              </p>
               <p>That's helpful. What can I do to improve my engagment?</p>
             </div>
           </div>
         </div>
       </div>
-      <div className="bg-sidebarcolor min-h-52 rounded-lg py-6 px-4 mt-4 mb-2">
-        <div className="flex justify-between gap-4">
-          <Avatar className=""/>
+      <div className="bg-sidebarcolor min-h-52 rounded-lg py-6 px-4 mt-4 mb-2 md:w-[80%]">
+        <div className="flex gap-4">
+          <Avatar className="" />
           <div className="flex gap-2">
             <div>
-              <p className="text-auth-text-color font-bold text-[13px] ">VISION</p>
+              <p className="text-auth-text-color font-bold text-[13px] ">
+                VISION
+              </p>
               {/* <small>Today 2:45 PM</small> */}
               <p>Thank you your instagram is connected now</p>
               <p>Analyzing ...</p>
-              <p>Analysis completed! Here's an overview of your recent performance:</p>
+              <p>
+                Analysis completed! Here's an overview of your recent
+                performance:
+              </p>
               <ul>
                 <li>Engagment rate : 4.5%</li>
-                <li>Top performaing Post : Your recent travel blog with 1200 likes and 300 comments</li>
-                <li>Audience Demographics: Predominantly aged 18-24 with a strong following from New York and Los Angeles</li>
+                <li>
+                  Top performaing Post : Your recent travel blog with 1200 likes
+                  and 300 comments
+                </li>
+                <li>
+                  Audience Demographics: Predominantly aged 18-24 with a strong
+                  following from New York and Los Angeles
+                </li>
               </ul>
               <div className="flex mt-4 gap-4">
                 <CopyOutlined />
@@ -66,22 +127,46 @@ const VisionUserChat = () => {
           </div>
         </div>
       </div>
-      <div className="border flex flex-col justify-between border-linkColor min-h-24 w-full rounded-xl p-3 mt-10 md:w-[80%] mx-auto">
-        <p className="text-auth-text-color text-sm ">Message VISION</p>
-        <p className="opacity-5 min-h-24 bg-custom-green-standard w-full my-1"></p>
-        <div className="flex justify-between items-center">
-          <div className="flex items-center justify-around gap-2">
-            <PaperClipOutlined className="text-lg"/>
-            <PictureOutlined className="text-lg"/>
-            <AudioOutlined className="text-lg"/>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="border flex flex-col justify-between border-linkColor min-h-24 w-full rounded-xl p-3 mt-10 md:w-[80%]"
+        >
+          <p className="text-auth-text-color text-sm ">Message VISION</p>
+          <FormField
+            control={form.control}
+            name="prompt"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Textarea
+                    placeholder="Message"
+                    className="resize-none focus:outline-none focus:ring-0 focus:border-transparent border-none focus-visible:ring-0 focus-visible:ring-offset-0 bg-opacity-25 min-h-24 bg-custom-green-standard w-full my-1 text-custom-green-night placeholder:text-custom-green-night"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <div className="flex justify-between items-center">
+            <div className="flex items-center justify-around gap-2">
+              <PaperClipOutlined className="text-lg" />
+              <PictureOutlined className="text-lg" />
+              <AudioOutlined className="text-lg" />
+            </div>
+            <Button
+              type="submit"
+              className=" hover:bg-auth-text-color bg-auth-text-color py-1 px-4 text-white rounded-lg"
+            >
+              <SendOutlined rotate={-45} />
+            </Button>
           </div>
-          <div className=" bg-auth-text-color py-1 px-2 text-white rounded-lg">
-            <SendOutlined rotate={-45}/>
-          </div>
-        </div>
-      </div>
+        </form>
+      </Form>
     </div>
   );
-}
- 
+};
+
 export default VisionUserChat;
